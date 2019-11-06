fbfd1a9c5db5467587f7386dba872b54d7f6088f
Harry Sild <puhhbaer@hotmail.com>
2019-11-06 20:54:55 +0200

    add mute button functionality

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
 import StartGameScreen from "./src/screens/StartGameScreen";
 import GameBoardScreen from "./src/screens/GameBoardScreen";
 import ScoreBoardScreen from "./src/screens/ScoreBoardScreen";
+import { playMusic } from "./src/gameLogic";
 
 const navigator = createStackNavigator(
   {
@@ -21,5 +22,7 @@ const navigator = createStackNavigator(
 const App = createAppContainer(navigator);
 
 export default () => {
-  return <App />;
+  const [playMusic, setPlayMusic] = useState(false);
+
+  return <App screenProps={{ playMusic, setPlayMusic }} />;
 };
--- src/screens/StartGameScreen.tsx
+++ src/screens/StartGameScreen.tsx
@@ -1,31 +1,34 @@
 import React, { useEffect } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
 import { NavigationStackProp } from "react-navigation-stack";
 import { playMusic } from "../gameLogic";
+import { Octicons } from "@expo/vector-icons";
 
 interface Props {
   navigation: NavigationStackProp;
+  screenProps: any;
 }
 
-const StartGame: React.FC<Props> = ({ navigation }) => {
+const StartGame: React.FC<Props> = ({ navigation, screenProps }) => {
   useEffect(() => {
     playMusic(true);
   }, []);
 
   return (
     <View style={styles.creenContainer}>
-      <Button
-        title="Play"
+      <Text>{screenProps.playMusic}</Text>
+      <TouchableOpacity
         onPress={() => {
-          playMusic(true);
+          playMusic(screenProps.playMusic);
+          screenProps.setPlayMusic(!screenProps.playMusic);
         }}
-      />
-      <Button
-        title="Stop"
-        onPress={() => {
-          playMusic(false);
-        }}
-      />
+      >
+        {!!screenProps.playMusic ? (
+          <Octicons name="mute" size={32} color="black" />
+        ) : (
+          <Octicons name="unmute" size={32} color="black" />
+        )}
+      </TouchableOpacity>
       <Button
         title="Go to Game Board Screen"
         onPress={() => {
