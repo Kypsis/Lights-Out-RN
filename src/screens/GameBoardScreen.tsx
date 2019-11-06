b6e3a456d202c2815f30f84995c8f0d841de4db5
Harry Sild <puhhbaer@hotmail.com>
2019-11-06 11:22:16 +0200

    add functionality to create a solvable board

--- src/screens/GameBoardScreen.tsx
+++ src/screens/GameBoardScreen.tsx
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { NavigationStackProp } from "react-navigation-stack";
 
 interface Props {
   navigation: NavigationStackProp;
+  rows: number;
+  columns: number;
+  chanceLightStartsOn: number;
 }
 
-const StartGame: React.FC<Props> = ({ navigation }) => {
+const StartGame: React.FC<Props> = ({
+  navigation,
+  rows = 5,
+  columns = 5,
+  chanceLightStartsOn = 0.2
+}) => {
+  const [hasWon, setHasWon] = useState(false);
+  const [board, setBoard] = useState([]);
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  const solvable = board => {
+    let qp1 = 0;
+    let qp2 = 0;
+    board.forEach((row, i) => {
+      row.forEach((cell, j) => {
+        cell && i !== 2 && j % 2 === 0 && qp1++;
+        cell && i % 2 === 0 && j !== 2 && qp2++;
+      });
+    });
+    return qp1 % 2 === 1 || qp2 % 2 === 1 ? false : true;
+  };
+
+  const createBoard = () => {
+    const newBoard = [...Array(rows)].map(rowItem =>
+      [...Array(columns)].map(
+        columnItem => Math.random() <= chanceLightStartsOn
+      )
+    );
+
+    console.log(solvable(newBoard));
+
+    solvable(newBoard) ? setBoard(newBoard) : createBoard();
+  };
+
   return (
     <View style={styles.ScreenContainer}>
       <Button
